577cfd3c2f763e8db03875f0d1bb8f041feadc65
Honey Gt <nadhigutu17@gmail.com>
2023-08-03 19:39:38 -0700

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,27 +13,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-		left = 1 + binary_tree_height_balance(tree->left);
+		left = 1 + binary_tree_balance(tree->left);
 	if (tree->right)
-		right = 1 + binary_tree_height_balance(tree->right);
+		right = 1 + binary_tree_balance(tree->right);
 	return (left - right);
-}
-
-/**
- * binary_tree_height_balance - measures the height of a binary tree
- * @tree: binary tree
- * Return: height
- */
-size_t binary_tree_height_balance(const binary_tree_t *tree)
-{
-	size_t left = 0;
-	size_t right = 0;
-
-	if (!tree)
-		return (0);
-	if (tree->left)
-		left = 1 + binary_tree_height_balance(tree->left);
-	if (tree->right)
-		right = 1 + binary_tree_height_balance(tree->right);
-	return ((left > right) ? left : right);
 }
